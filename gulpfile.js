67a884695463e2f745197b0b69487a11e4c7e947
Guilherme Santos <ggs12@inf.ufpr.br>
2018-02-02 16:26:38 -0200

    add partials and browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,34 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
-  return gulp.src('./src/styles/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/styles/'));
+	return gulp.src('./src/styles/main.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: [
+				'> 1%',
+				'last 2 versions',
+				'firefox >= 4',
+				'safari 7',
+				'safari 8',
+				'IE 8',
+				'IE 9',
+				'IE 10',
+				'IE 11'
+			],
+			cascade: false
+		}))
+	.pipe(gulp.dest('./dist/styles/'))
+	.pipe(browserSync.stream());
 });
 
-gulp.task('watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('start', ['sass'], function () {
+	 browserSync.init({
+        server: "./"
+    });
+
+	gulp.watch('./src/styles/**/*.scss', ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
