71ee975597008a09f2c84991d9a90a4d35ace347
Guilherme Santos <ggs12@inf.ufpr.br>
2018-02-02 21:28:01 -0200

    added scripts structure and start header

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('sass', function () {
 	return gulp.src('./src/styles/main.scss')
@@ -24,11 +26,21 @@ gulp.task('sass', function () {
 	.pipe(browserSync.stream());
 });
 
+gulp.task('scripts', function(){
+  return gulp.src([ // The order that you list the files in this array IS IMPORTANT!!
+      'src/scripts/main.js',
+    ])
+  .pipe(concat('scripts.min.js')) // concatenates the JS files listed above into one file called scripts.min.js
+  .pipe(uglify()) // minifies scripts.min.js
+  .pipe(gulp.dest('dist/scripts/')) // tells task which directory to outputs uglified (minified) scripts.min.js
+});
+
 gulp.task('start', ['sass'], function () {
 	 browserSync.init({
         server: "./"
     });
 
 	gulp.watch('./src/styles/**/*.scss', ['sass']);
+	gulp.watch('src/scripts/**.js', ['scripts']); // Watch the JS files for changes
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
