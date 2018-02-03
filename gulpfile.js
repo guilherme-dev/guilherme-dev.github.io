d9dd8ad9df3673f6630bd7088589f0c5e634de31
Guilherme Santos <ggs12@inf.ufpr.br>
2018-02-03 16:12:59 -0200

    navbar and navbar__button

--- gulpfile.js
+++ gulpfile.js
@@ -10,15 +10,15 @@ gulp.task('sass', function () {
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: [
-				'> 1%',
-				'last 2 versions',
-				'firefox >= 4',
-				'safari 7',
-				'safari 8',
-				'IE 8',
-				'IE 9',
-				'IE 10',
-				'IE 11'
+				// '> 1%',
+				'last 2 versions'
+				// 'firefox >= 4',
+				// 'safari 7',
+				// 'safari 8',
+				// 'IE 8',
+				// 'IE 9',
+				// 'IE 10',
+				// 'IE 11'
 			],
 			cascade: false
 		}))
